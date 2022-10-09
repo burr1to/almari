fa7f1a0ad2258d10f1a6521d6327eebcba31bb7d
burr1to <singhsanskar240@gmail.com>
2022-10-09 17:54:48 +0545

    individual

--- src/views/IndividualItems/Individual.jsx
+++ src/views/IndividualItems/Individual.jsx
@@ -2,18 +2,46 @@ import React from "react";
 import Layout from "./../../components/global/Layout";
 import "./statics/css/individual.css";
 import Grid from "@mui/material/Grid";
+import Image from "./../../components/global/Image";
+import photo from "./pexel.jpg";
 function individual() {
   return (
     <Layout>
-      <Grid
-        container
-        direction='column'
-        justifyContent='space-around'
-        alignItems='center'
-      >
-        <Grid item className='individual-photo'></Grid>
-        <Grid item></Grid>
-      </Grid>
+      <br />
+      <div className='individual-product-page'>
+        <div className='product-grid'>
+          <div className='img-container'>
+            <Image src={photo} className='main_image img-for-product' />
+          </div>
+          <div className='info-container'>
+            <ul className='basic-info'>
+              <li className='title'>Ding Ding Ding Ninja Store</li>
+              <li className='price'>$20</li>
+            </ul>
+            <br />
+            <div className='company-info'>
+              <p className='company-name'>Hattori Enterprises</p>
+              <p className='review'>Review</p>
+            </div>
+            <p className='description'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <button>Add to cart</button>
+            <div className='product-highlights'>
+              <h2>Highlights</h2>
+              <ul>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
