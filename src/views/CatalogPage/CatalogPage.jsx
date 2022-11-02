bba6746d40b23d284d78a0c96ef0b9f213e2a004
burr1to <singhsanskar240@gmail.com>
2022-11-02 09:43:52 +0545

    catalog

--- src/views/CatalogPage/CatalogPage.jsx
+++ src/views/CatalogPage/CatalogPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./statics/css/catalog.css";
+import Layout from "./../../components/global/Layout";
 
 function CatalogPage() {
-  return <div>CatalogPage</div>;
+  return (
+    <>
+      <Layout>
+        <div className='title-area'>Title area</div>
+        <div className='catalog-area'>Catalog Area</div>
+      </Layout>
+    </>
+  );
 }
 
 export default CatalogPage;
